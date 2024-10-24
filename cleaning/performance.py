2b30505903f81d5706f2d9e6a0dd3956e8a6a9f0
Dang Nhat <88953246+dangnhatsimon@users.noreply.github.com>
2024-10-25 05:35:46 +0700

    Add files via upload

---
+++ cleaning/performance.py
@@ -0,0 +1,113 @@
+# Import the pyspark.sql.types library
+from pyspark.sql.types import *
+from pyspark.sql import SparkSession 
+from pyspark import SparkConf
+from pyspark import SparkContext
+import pyspark.sql.functions as F
+import time
+sc = SparkContext.getOrCreate(SparkConf().setMaster("local[*]"))
+spark = SparkSession(sc)
+
+
+start_time = time.time()
+
+# Add caching to the unique rows in departures_df
+departures_df = departures_df.distinct().cache()
+
+# Count the unique rows in departures_df, noting how long the operation takes
+print("Counting %d rows took %f seconds" % (departures_df.count(), time.time() - start_time))
+
+# Count the rows again, noting the variance in time of a cached DataFrame
+start_time = time.time()
+print("Counting %d rows again took %f seconds" % (departures_df.count(), time.time() - start_time))
+
+
+# Determine if departures_df is in the cache
+print("Is departures_df cached?: %s" % departures_df.is_cached)
+print("Removing departures_df from cache")
+
+# Remove departures_df from the cache
+departures_df.unpersist()
+
+# Check the cache status again
+print("Is departures_df cached?: %s" % departures_df.is_cached)
+
+
+# Import the full and split files into DataFrames
+full_df = spark.read.csv('departures_full.txt.gz')
+split_df = spark.read.csv('departures_0*.txt.gz')
+
+# Print the count and run time for each DataFrame
+start_time_a = time.time()
+print("Total rows in full DataFrame:\t%d" % full_df.count())
+print("Time to run: %f" % (time.time() - start_time_a))
+
+start_time_b = time.time()
+print("Total rows in split DataFrame:\t%d" % split_df.count())
+print("Time to run: %f" % (time.time() - start_time_b))
+
+
+
+# Name of the Spark application instance
+app_name = spark.conf.get("spark.app.name")
+
+# Driver TCP port
+driver_tcp_port = spark.conf.get("spark.driver.port")
+
+# Number of join partitions
+num_partitions = spark.conf.get('spark.sql.shuffle.partitions')
+
+# Show the results
+print("Name: %s" % app_name)
+print("Driver TCP port: %s" % driver_tcp_port)
+print("Number of partitions: %s" % num_partitions)
+
+
+# Store the number of partitions in variable
+before = departures_df.rdd.getNumPartitions()
+
+# Configure Spark to use 500 partitions
+spark.conf.set('spark.sql.shuffle.partitions', 500)
+
+# Recreate the DataFrame using the departures data file
+departures_df = spark.read.csv('departures.txt.gz').distinct()
+
+# Print the number of partitions for each instance
+print("Partition count before change: %d" % before)
+print("Partition count after change: %d" % departures_df.rdd.getNumPartitions())
+
+
+
+# Join the flights_df and aiports_df DataFrames
+normal_df = flights_df.join(airports_df, \
+    flights_df["Destination Airport"] == airports_df["IATA"] )
+
+# Show the query plan
+normal_df.explain()
+
+
+
+# Import the broadcast method from pyspark.sql.functions
+from pyspark.sql.functions import broadcast
+
+# Join the flights_df and airports_df DataFrames using broadcasting
+broadcast_df = flights_df.join(broadcast(airports_df), \
+    flights_df["Destination Airport"] == airports_df["IATA"] )
+
+# Show the query plan and compare against the original
+broadcast_df.explain()
+
+
+start_time = time.time()
+# Count the number of rows in the normal DataFrame
+normal_count = normal_df.count()
+normal_duration = time.time() - start_time
+
+start_time = time.time()
+# Count the number of rows in the broadcast DataFrame
+broadcast_count = broadcast_df.count()
+broadcast_duration = time.time() - start_time
+
+# Print the counts and the duration of the tests
+print("Normal count:\t\t%d\tduration: %f" % (normal_count, normal_duration))
+print("Broadcast count:\t%d\tduration: %f" % (broadcast_count, broadcast_duration))
